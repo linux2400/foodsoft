a3ae0e1bf8ae4009b613963e286aa37ca56fe2bb
wvengen <dev-voko@willem.engen.nl>
2014-06-02 12:01:34 +0200

    fix bigdecimal display hack

--- config/initializers/number_display.rb
+++ config/initializers/number_display.rb
@@ -7,11 +7,16 @@ class Float
   end
 end
 
+# allow +to_s+ on bigdecimal without argument too
 if defined? BigDecimal
   class BigDecimal
     alias :foodsoft_to_s :to_s
-    def to_s(format = DEFAULT_STRING_FORMAT)
-      foodsoft_to_s(format).chomp(".0")
+    def to_s(format = nil)
+      if format.nil?
+        foodsoft_to_s(DEFAULT_STRING_FORMAT).chomp(".0")
+      else
+        foodsoft_to_s(format)
+      end
     end
   end
 end
